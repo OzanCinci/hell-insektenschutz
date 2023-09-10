c77118e4ca3808bdd40a536582faace91f044c67
OzanCinci <ozan_cinci2001@hotmail.com>
2023-09-11 00:53:58 +0300

    delete lammerman key from data

--- fe/src/DetailPages/Data.js
+++ fe/src/DetailPages/Data.js
@@ -115,7 +115,7 @@ export const data = {
         spannrahmen : {
             welcoming : {
                 title: "Spannrahmen - hochwertig & passgenau",
-                desc: "Lämmermann Spannrahmen sind die erste Wahl für Fenster und Türen, die rein zum Lüften geöffnet werden. Der große Vorteil: unsere Montage erfolgt in der Regel ohne Bohren."
+                desc: "Spannrahmen sind die erste Wahl für Fenster und Türen, die rein zum Lüften geöffnet werden. Der große Vorteil: unsere Montage erfolgt in der Regel ohne Bohren."
             },
             data_array: [
                 {
@@ -237,12 +237,12 @@ export const data = {
         rollo : {
             welcoming : {
                 title: "Rollo – dezenter & flexibler Insektenschutz nach Bedarf",
-                desc: "Lämmermann Rollos werden gerne im Bereich von Fenstern verbaut, wenn ein bedarfsgerechter Insektenschutz gewünscht ist. Auch mit elektrischem Antrieb verfügbar."
+                desc: "Rollos werden gerne im Bereich von Fenstern verbaut, wenn ein bedarfsgerechter Insektenschutz gewünscht ist. Auch mit elektrischem Antrieb verfügbar."
             },
             data_array: [
                 {
                     title: "Ästhetik im Blick",
-                    text: "Wer das neue Lämmermann Insektenschutz-Rollo einmal bedient hat, verfällt schnell seiner Wertigkeit. Die Zugschiene mit integrierter Griffmulde verleiht dem Element eine edle Haptik. Mit einem leichten Druck auf die Zugschiene löst sich das Rollo aus dem Verschluss und wickelt sich flüsterleise ganz sanft zurück in die Kassette. Die farblich auf einander abgestimmten Profile lassen das Rollo dezent im Fassadenbild wirken.",
+                    text: "Wer das neue Insektenschutz-Rollo einmal bedient hat, verfällt schnell seiner Wertigkeit. Die Zugschiene mit integrierter Griffmulde verleiht dem Element eine edle Haptik. Mit einem leichten Druck auf die Zugschiene löst sich das Rollo aus dem Verschluss und wickelt sich flüsterleise ganz sanft zurück in die Kassette. Die farblich auf einander abgestimmten Profile lassen das Rollo dezent im Fassadenbild wirken.",
                     img: Rollos1,
                     def: true,
                 },
@@ -298,7 +298,7 @@ export const data = {
         plissee : {
             welcoming : {
                 title: "Plissee für Fenster – der schicke & flexible Insektenschutz",
-                desc: "Sie suchen eine Insektenschutzlösung, die platzsparend zusammen-gefaltet werden kann? Die Lämmermann-Plissees können mit einer Hand bedient werden und sind dabei extrem schick und langlebig."
+                desc: "Sie suchen eine Insektenschutzlösung, die platzsparend zusammen-gefaltet werden kann? Die Plissees können mit einer Hand bedient werden und sind dabei extrem schick und langlebig."
             },
             data_array: [
                 {
@@ -349,7 +349,7 @@ export const data = {
         drehtür : {
             welcoming : {
                 title: "Drehtür – stabiler & eleganter Insektenschutz",
-                desc: "Lämmermann Insektenschutz-Drehtüren werden im Bereich von Terrassen- und Balkontüren eingesetzt. Durch die stabile Konstruktion erfüllt der Rahmen höchste Ansprüche an Solidität."
+                desc: "Insektenschutz-Drehtüren werden im Bereich von Terrassen- und Balkontüren eingesetzt. Durch die stabile Konstruktion erfüllt der Rahmen höchste Ansprüche an Solidität."
             },
             data_array: [
                 {
@@ -366,7 +366,7 @@ export const data = {
                 },
                 {
                     title: "Trittschutzblech – für die richtige Stabilität",
-                    text: "Bereits im Standard stattet Lämmermann alle Insektenschutz-Drehtüren mit einem Trittschutzblech an der Elementunterseite aus. Die Aussteifung verleiht dem Insektenschutzelement zusätzliche Stabilität und ist zudem praktisch, da in diesem Bereich der Rahmen mit sanfter Fußbedienung geöffnet werden kann.",
+                    text: "Bereits im Standard stattet alle Insektenschutz-Drehtüren mit einem Trittschutzblech an der Elementunterseite aus. Die Aussteifung verleiht dem Insektenschutzelement zusätzliche Stabilität und ist zudem praktisch, da in diesem Bereich der Rahmen mit sanfter Fußbedienung geöffnet werden kann.",
                     img: Drehtür3,
                     def: true,
                 },
@@ -423,7 +423,7 @@ export const data = {
         pendeltür : {
             welcoming : {
                 title: "Pendeltür – maximaler Komfort",
-                desc: "Insektenschutz-Pendeltüren von Lämmermann vereinen Funktion und Design in Perfektion. Pendeltüren öffnen in zwei Richtungen und schließen von selbst."
+                desc: "Insektenschutz-Pendeltüren von vereinen Funktion und Design in Perfektion. Pendeltüren öffnen in zwei Richtungen und schließen von selbst."
             },
             data_array: [
                 {
@@ -440,7 +440,7 @@ export const data = {
                 },
                 {
                     title: "Sockelblech – für die richtige Stabilität",
-                    text: "Bereits im Standard stattet Lämmermann alle Insektenschutz-Pendelrahmen mit einem Sockelblech an der Elementunterseite aus. Die Aussteifung verleiht dem Insektenschutzelement zusätzliche Stabilität und ist zudem praktisch, da in diesem Bereich der Rahmen mit sanfter Fußbedienung geöffnet werden kann.",
+                    text: "Bereits im Standard stattet alle Insektenschutz-Pendelrahmen mit einem Sockelblech an der Elementunterseite aus. Die Aussteifung verleiht dem Insektenschutzelement zusätzliche Stabilität und ist zudem praktisch, da in diesem Bereich der Rahmen mit sanfter Fußbedienung geöffnet werden kann.",
                     img: Pendaltür3,
                     def: false,
                 },
@@ -502,7 +502,7 @@ export const data = {
             data_array: [
                 {
                     title: "Leichtgängigkeit",
-                    text: "Lämmermann Schiebetüren mit angenehm gestalteten Profilkonturen und hochtransparentem Gewebe sind ein echter Hingucker – sofern Ihre Gäste den Insektenschutz überhaupt bemerken. Laufruhe, Leichtgängigkeit und eine intuitive Bedienung zeichnen unsere Schiebesysteme aus. Mit der integrierten Griffnut kann der Flügel in jeder Angriffshöhe geöffnet werden, dies ist auch ideal für Kinder. Mit dem im Standard verbauten Schließbeschlag arretiert Ihr Flügel sanft in Endposition. Unsere Gewebe zeichnen sich durch hohe Durchsicht bei geprüfter Langlebigkeit aus – überzeugen Sie sich.",
+                    text: "Schiebetüren mit angenehm gestalteten Profilkonturen und hochtransparentem Gewebe sind ein echter Hingucker – sofern Ihre Gäste den Insektenschutz überhaupt bemerken. Laufruhe, Leichtgängigkeit und eine intuitive Bedienung zeichnen unsere Schiebesysteme aus. Mit der integrierten Griffnut kann der Flügel in jeder Angriffshöhe geöffnet werden, dies ist auch ideal für Kinder. Mit dem im Standard verbauten Schließbeschlag arretiert Ihr Flügel sanft in Endposition. Unsere Gewebe zeichnen sich durch hohe Durchsicht bei geprüfter Langlebigkeit aus – überzeugen Sie sich.",
                     img: Schiebetür1,
                     def: true,
                 },
@@ -563,7 +563,7 @@ export const data = {
             data_array: [
                 {
                     title: "Zeitgeist",
-                    text: "Klare Linien und breite Öffnungen spiegeln den Zeitgeist der modernen Architektur wider. Lämmermann-Plissees wurden für besonders anspruchsvolle Bedürfnisse an Design und Funktion entwickelt. Mit einer Einbautiefe von lediglich 22 mm und einer extra flachen Bodenschiene entsteht eine nahezu nahtlose Integration zwischen Insektenschutz und Ihrem bestehenden Fensterelement. In Funktion entfaltet das Plissee seine volle Wirkung und sorgt für insektenfreien Wohnraum – zusammengeschoben benötigt es jedoch nur minimalen Platz.",
+                    text: "Klare Linien und breite Öffnungen spiegeln den Zeitgeist der modernen Architektur wider. Plissees wurden für besonders anspruchsvolle Bedürfnisse an Design und Funktion entwickelt. Mit einer Einbautiefe von lediglich 22 mm und einer extra flachen Bodenschiene entsteht eine nahezu nahtlose Integration zwischen Insektenschutz und Ihrem bestehenden Fensterelement. In Funktion entfaltet das Plissee seine volle Wirkung und sorgt für insektenfreien Wohnraum – zusammengeschoben benötigt es jedoch nur minimalen Platz.",
                     img: Plissee1,
                     def: true,
                 },
@@ -575,7 +575,7 @@ export const data = {
                 },
                 {
                     title: "Vertikale Zugschiene – für die richtige Ergonomie",
-                    text: "Die mit einer tiefen Eingriffnut entwickelte Zugschiene ermöglicht auch für große Hände eine gute Bedienung. Besonders praktisch ist, dass die Zugschiene des Lämmermann-Plissees vertikal durchgehend verläuft und somit auf jeder Angriffshöhe bedient werden kann.",
+                    text: "Die mit einer tiefen Eingriffnut entwickelte Zugschiene ermöglicht auch für große Hände eine gute Bedienung. Besonders praktisch ist, dass die Zugschiene des Plissees vertikal durchgehend verläuft und somit auf jeder Angriffshöhe bedient werden kann.",
                     img: Plissee3,
                     def: true,
                 },
@@ -637,18 +637,18 @@ export const data = {
         gitterSafe : {
             welcoming : {
                 title: "Drehtür – stabiler & eleganter Insektenschutz",
-                desc: "Die Lämmermann GitterSafe-Reihe erfüllt höchste Ansprüche an Design und Komfort. Durch die flächenbündige Einbettung entstehen keine Stolperkanten."
+                desc: "Die GitterSafe-Reihe erfüllt höchste Ansprüche an Design und Komfort. Durch die flächenbündige Einbettung entstehen keine Stolperkanten."
             },
             data_array: [
                 {
                     title: "Ästhetik im Blick",
-                    text: "Der Lämmermann-Gittersafe ist ein Sandwichelement, das Ihren bestehenden Gitterrost komplett ersetzt. Die dann neu eingesetzte Abdeckung ist belastbar und bietet zudem Schutz vor Spinnen und Laub. Besonders schön ist, dass das gesamte Element wie aus einem Guss ist und flächenbündig zur Umgebung integriert werden kann.",
+                    text: "Der Gittersafe ist ein Sandwichelement, das Ihren bestehenden Gitterrost komplett ersetzt. Die dann neu eingesetzte Abdeckung ist belastbar und bietet zudem Schutz vor Spinnen und Laub. Besonders schön ist, dass das gesamte Element wie aus einem Guss ist und flächenbündig zur Umgebung integriert werden kann.",
                     img: GitterSafe1,
                     def: true,
                 },
                 {
                     title: "GFK und Edelstahl",
-                    text: "Freude am Insektenschutz – ein Produktleben lang. Der Lämmermann-Gittersafe ist ein Sandwichelement, das Ihren bestehenden Gitterrost komplett ersetzt. Das Rahmenelement besteht aus hochbelastbaren Konstruktionsprofilen, die zu einem Rahmen verpresst werden. Herzstück des Ganzen ist ein passgenau eingelassener GFK-Gitterrost und ein 0,6 mm dickes Edelstahlgewebe, das vor Kleintieren und Laub schützt.",
+                    text: "Freude am Insektenschutz – ein Produktleben lang. Der Gittersafe ist ein Sandwichelement, das Ihren bestehenden Gitterrost komplett ersetzt. Das Rahmenelement besteht aus hochbelastbaren Konstruktionsprofilen, die zu einem Rahmen verpresst werden. Herzstück des Ganzen ist ein passgenau eingelassener GFK-Gitterrost und ein 0,6 mm dickes Edelstahlgewebe, das vor Kleintieren und Laub schützt.",
                     img: GitterSafe2,
                     def: false,
                 },
@@ -667,7 +667,7 @@ export const data = {
             data_array: [
                 {
                     title: "Ästhetik im Blick",
-                    text: "Wer das neue Lämmermann Insektenschutz-Rollo einmal bedient hat, verfällt schnell seiner Wertigkeit. Die Zugschiene mit integrierter Griffmulde verleiht dem Element eine edle Haptik. Mit einem leichten Druck auf die Zugschiene löst sich das Rollo aus dem Verschluss und wickelt sich flüsterleise ganz sanft zurück in die Kassette. Die farblich auf einander abgestimmten Profile lassen das Rollo dezent im Fassadenbild wirken.",
+                    text: "Wer das neue Insektenschutz-Rollo einmal bedient hat, verfällt schnell seiner Wertigkeit. Die Zugschiene mit integrierter Griffmulde verleiht dem Element eine edle Haptik. Mit einem leichten Druck auf die Zugschiene löst sich das Rollo aus dem Verschluss und wickelt sich flüsterleise ganz sanft zurück in die Kassette. Die farblich auf einander abgestimmten Profile lassen das Rollo dezent im Fassadenbild wirken.",
                     img: DachRollo1,
                     def: false,
                 },
@@ -683,7 +683,7 @@ export const data = {
         plissee : {
             welcoming : {
                 title: "Plissee für Dachfenster – der praktische & dezente Insektenschutz",
-                desc: "Flexible Insektenschutzlösung, die platzsparend zusammengefaltet werden kann. Die Lämmermann-Plissees für Dachfenster können mit einer Hand bedient werden und sind dabei schick und langlebig."
+                desc: "Flexible Insektenschutzlösung, die platzsparend zusammengefaltet werden kann. Die Plissees für Dachfenster können mit einer Hand bedient werden und sind dabei schick und langlebig."
             },
             data_array: [
                 {
